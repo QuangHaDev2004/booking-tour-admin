7db49a71f435f4537ea687ef92fc24df05898e08
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-11-25 13:03:05 +0700

    feat: account admin edit

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,4 +1,22 @@
-export const ACTIVE_STATUS_LIST = [
-  { label: "Hoạt động", value: "active" },
-  { label: "Tạm dừng", value: "inactive" },
+export const statusList = [
+  {
+    label: "Hoạt động",
+    value: "active",
+    color: "#00b69b",
+    colorBg: "rgba(0, 182, 155, 0.2)",
+  },
+  {
+    label: "Tạm dừng",
+    value: "inactive",
+    color: "#ef3826",
+    colorBg: "rgba(239, 56, 38, 0.2)",
+  },
+  {
+    label: "Khởi tạo",
+    value: "initial",
+    color: "#ffa756",
+    colorBg: "rgba(255, 167, 86, 0.2)",
+  },
 ];
+
+export const ACTIVE_STATUS_LIST = []
